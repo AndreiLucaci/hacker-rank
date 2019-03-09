6ee7034cfdbb2a9bfb83cca7bf7516cbb49c82b4
Andrei Lucaci <contact@andreilucaci.ro>
2019-03-09 15:56:17 +0200

    StrongPassowrd problem

---
+++ HackerRank/ProblemInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using HackerRank.Prcatice.Algoritms.Strings;
+
+namespace HackerRank
+{
+    public class ProblemInitializer
+    {
+        public static Dictionary<string, ISolution> Problems = new Dictionary<string, ISolution>
+        {
+            [Pratice.Algoritms.Strings.StrongPassowrd] = new StrongPassword()
+        };
+    }
+
+    public class Pratice
+    {
+        public class Algoritms
+        {
+            public class Strings
+            {
+                public const string StrongPassowrd = nameof(StrongPassowrd);
+            }
+        }
+    }
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var problem = ProblemInitializer.Problems[Pratice.Algoritms.Strings.StrongPassowrd];
+
+            problem.Solve();
+        }
+    }
+}
